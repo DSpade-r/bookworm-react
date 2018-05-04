dfa4da2d4b915c0e641a8672cf978fa0a5e89d79
Denis Lopatin <stalk.sdv@gmail.com>
2018-05-04 18:41:30 +0300

    Login logout

--- src/api.js
+++ src/api.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 export default {
     user: {
         login: (credentials) => 
+            // рабочий вызов для примера
             axios.post('/api/auth', {credentials}).then(res => res.data.user)
+            // рабочий вызов для Airvector API local
+            // axios.get('http://localhost:55488/api.svc/denis/23', {credentials}).then(res => res.data.user)
+            // рабочий вызов для Airvector API
+            // axios.post('https://api.airvector-hvac.com/api.svc/auth', {user : credentials.email, pwd: credentials.password}).then(res => res.data.user)
     }
 }
--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -18,7 +18,7 @@ class LoginForm extends React.Component {
 
     onChange = e => this.setState({
         data: {...this.state.data, [e.target.name]: e.target.value}
-    });
+    }); 
 
     onSubmit = () => {
         const errors = this.validate(this.state.data);
@@ -45,6 +45,7 @@ class LoginForm extends React.Component {
             {errors.global && 
             <Message negative>
                 <Message.Header>Something went wrong</Message.Header>
+                <p>{errors.global}</p>
             </Message>}
             <Form.Field error={!!errors.email}>
                 <label htmlFor="email">Email</label>
